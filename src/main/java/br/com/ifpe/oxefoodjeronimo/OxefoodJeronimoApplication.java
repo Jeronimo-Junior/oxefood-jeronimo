b6d2f35a125095d8136f53999fa0146690ef81c7
aluno <aluno@ifpe>
2022-09-29 09:19:42 -0300

    Arquivos Java

---
+++ src/main/java/br/com/ifpe/oxefoodjeronimo/OxefoodJeronimoApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefoodjeronimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodJeronimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodJeronimoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/oxefoodjeronimo/OxefoodJeronimoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefoodjeronimo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxefoodJeronimoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
